d315136b166b1080b250fcb2e35208d2c50a08cb
Lurgrid <Lurgrid@gmail.com>
2022-12-06 10:11:05 +0100

    Init

---
+++ src/stack.c
@@ -0,0 +1,97 @@
+//  stack.c : partie implantation d'un module polymorphe pour la spécification
+//    PILE du TDA Pile(T).
+
+#include "stack.h"
+#include <stdint.h>
+
+//  struct stack, stack : gestion des adresses des objets dans la pile à l'aide
+//    d'un tableau a taille dynamique. Le composant aref a pour valeur le
+//    pointeur du tableau de ref, le composant height mémorise la longueur de
+//    la liste. Le composant height a donc pour valeur la hauteur de la pile.
+//    et capacity a pour valeur la capacité actuel de la pile.
+
+#define DEFAULT_SIZE 32
+#define MULTI_STACK 2
+
+struct stack {
+  const void **aref;
+  size_t height;
+  size_t capacity;
+};
+
+stack *stack_empty(void) {
+  stack *p = malloc(sizeof *p);
+  if (p == NULL) {
+    return NULL;
+  }
+  p->aref = malloc(DEFAULT_SIZE * sizeof *(p->aref));
+  if (p->aref == NULL) {
+    free(p);
+    return NULL;
+  }
+  p->height = 0;
+  p->capacity = DEFAULT_SIZE;
+  return p;
+}
+
+void stack_dispose(stack **sptr) {
+  if (*sptr == NULL) {
+    return;
+  }
+  free((*sptr)->aref);
+  free(*sptr);
+  *sptr = NULL;
+}
+
+void *stack_push(stack *s, const void *ref) {
+  if (ref == NULL) {
+    return NULL;
+  }
+  if (s->height < s->capacity) {
+    s->aref[s->height] = ref;
+  } else {
+    if (MULTI_STACK > SIZE_MAX / (s->capacity * sizeof *(s->aref))) {
+      return NULL;
+    }
+    const void **t = realloc(s->aref,
+        s->capacity * sizeof *(s->aref) * MULTI_STACK);
+    if (t == NULL) {
+      return NULL;
+    }
+    s->capacity *= MULTI_STACK;
+    s->aref = t;
+    s->aref[s->height] = ref;
+  }
+  ++(s->height);
+  return (void *) ref;
+}
+
+void *stack_pop(stack *s) {
+  if (stack_is_empty(s)) {
+    return NULL;
+  }
+  --s->height;
+  return (void *) s->aref[s->height];
+}
+
+void *stack_top(stack *s) {
+  if (stack_is_empty(s)) {
+    return NULL;
+  }
+  return (void *) s->aref[s->height - 1];
+}
+
+void *stack_nth(stack *s, size_t n){
+  if (stack_is_empty(s) || n > s->height){
+    return NULL;
+  }
+  return (void *) s->aref[n - 1];
+}
+
+bool stack_is_empty(stack *s) {
+  return s->height == 0;
+}
+
+size_t stack_height(stack *s) {
+  return s->height;
+}
---
+++ src/stack.h
@@ -0,0 +1,76 @@
+//  stack.h : partie interface d'un module polymorphe pour la spécification PILE
+//    du TDA Pile(T).
+
+#ifndef STACK__H
+#define STACK__H
+
+//  Fonctionnement général :
+//  - la structure de données ne stocke pas d'objets mais des références vers
+//      ces objets. Les références sont du type générique « void * » ;
+//  - si des opérations d'allocation dynamique sont effectuées, elles le sont
+//      pour la gestion propre de la structure de données, et en aucun cas pour
+//      réaliser des copies ou des destructions d'objets ;
+//  - les fonctions qui possèdent un paramètre de type « stack * » ou
+//      « stack ** » ont un comportement indéterminé lorsque ce paramètre ou sa
+//      déréférence n'est pas l'adresse d'un contrôleur préalablement renvoyée
+//      avec succès par la fonction stack_empty et non révoquée depuis par la
+//      fonction stack_dispose ;
+//  - aucune fonction ne peut ajouter NULL à la structure de données ;
+//  - les fonctions de type de retour « void * » renvoient NULL en cas d'échec.
+//      En cas de succès, elles renvoient une référence actuellement ou
+//      auparavant stockée par la structure de données ;
+//  - l'implantation des fonctions dont la spécification ne précise pas qu'elles
+//      doivent gérer les cas de dépassement de capacité ne doivent avoir
+//      affaire avec aucun problème de la sorte.
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+//- STANDARD -------------------------------------------------------------------
+
+//  struct stack, stack : type et nom de type d'un contrôleur regroupant les
+//    informations nécessaires pour gérer une pile sur des objets quelconques.
+typedef struct stack stack;
+
+//  stack_empty : tente d'allouer les ressources nécessaires pour gérer une
+//    nouvelle pile initialement vide. Renvoie NULL en cas de dépassement de
+//    capacité. Renvoie sinon un pointeur vers le contrôleur associé à la pile.
+extern stack *stack_empty(void);
+
+//  stack_dispose : sans effet si *sptr vaut NULL. Libère sinon les ressources
+//    allouées à la gestion de la pile associée à *sptr puis affecte NULL à
+//    *sptr.
+extern void stack_dispose(stack **sptr);
+
+//  stack_push : renvoie NULL si ref vaut NULL. Tente sinon d'empiler ref sur la
+//    pile associée à s ; renvoie NULL en cas de dépassement de capacité ;
+//    renvoie sinon ref.
+extern void *stack_push(stack *s, const void *ref);
+
+//  stack_pop : renvoie NULL si la pile associée à s est vide. Dépile sinon la
+//    pile et renvoie la référence qui était au sommet de la pile avant le
+//    dépilement.
+extern void *stack_pop(stack *s);
+
+//  stack_pop : renvoie NULL si la pile associée à s est vide. Dépile sinon la
+//    pile et renvoie la référence qui était au sommet de la pile avant le
+//    dépilement.
+extern void *stack_pop(stack *s);
+
+//  stack_top : renvoie NULL si la pile associée à s est vide. Renvoie sinon
+//    la référence qui est au sommet de la pile.
+extern void *stack_top(stack *s);
+
+//  stack_nth : renvoie NULL si la pile associée à s est vide ou si n est
+//    supérieur a la hauteur de pile. Renvoie sinon la n-ème référence de la
+//    pile.
+extern void *stack_nth(stack *s, size_t n);
+
+//  stack_is_empty : renvoie true ou false selon que la pile associée à s est
+//    vide ou non.
+extern bool stack_is_empty(stack *s);
+
+//  stack_height : renvoie la hauteur de la pile associée à s.
+extern size_t stack_height(stack *s);
+
+#endif
